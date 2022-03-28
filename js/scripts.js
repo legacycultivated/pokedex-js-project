859f3eb397e6c36398bbee67fc1f26d4b5561f4a
Lyman Chan <87409209+legacycultivated@users.noreply.github.com>
2022-03-27 20:53:25 -0700

    Update scripts.js
    Added for loop that list pokemon with updated height and added <br>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 let pokemonList = [
-  {name: "Bulbasaur", height: 1, types: ['grass', 'poison']},
-  {name: "Charmander", height: 2, types: ['fire']},
-  {name: "Squirtle", height: 2, types: ['water']},
-  {name: "Snorlax", height: 5, types:['normal']}
+  {name: "Bulbasaur", height: 0.7, types: ['grass', 'poison']},
+  {name: "Charmander", height: 0.6, types: ['fire']},
+  {name: "Squirtle", height: 0.5, types: ['water']},
+  {name: "Snorlax", height: 2.1, types:['normal']}
 ];
+
+//for loop that prints out pokemon and height to index.html
+
+for (let i= 0; i < pokemonList.length; i++) {
+  document.write (pokemonList[i].name + ": " + pokemonList[i].height);
+
+  document.write ("<br>");
+}
