5229f5b2d414fcfede38f8adb7ce13e14b60fc25
Lyman Chan <87409209+legacycultivated@users.noreply.github.com>
2022-03-27 20:32:30 -0700

    Update scripts.js
    Updated height description and added another object to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-  {name: "Bulbasaur", number: 1, types: ['grass', 'poison']},
-  {name: "Charmander", number: 4, types: ['fire']},
-  {name: "Squirtle", number: 7, types: ['water']}  
+  {name: "Bulbasaur", height: 1, types: ['grass', 'poison']},
+  {name: "Charmander", height: 2, types: ['fire']},
+  {name: "Squirtle", height: 2, types: ['water']},
+  {name: "Snorlax", height: 5, types:['normal']}
 ];
