c37a8ffba2a817de49461a22f67df1763109225f
Lyman Chan <87409209+legacycultivated@users.noreply.github.com>
2022-03-27 23:03:29 -0700

    Update scripts.js
    organized code/indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
-let pokemonList = [
-  {name: "Bulbasaur", height: 0.7, types: ['grass', 'poison']},
-  {name: "Charmander", height: 0.6, types: ['fire']},
-  {name: "Squirtle", height: 0.5, types: ['water']},
-  {name: "Snorlax", height: 2.1, types:['normal']},
-  {name: "Eevee", height: 0.3, types: ['normal']}
+let pokemonList = [{
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: ['fire']
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ['water']
+  },
+  {
+    name: "Snorlax",
+    height: 2.1,
+    types: ['normal']
+  },
+  {
+    name: "Eevee",
+    height: 0.3,
+    types: ['normal']
+  }
 ];
 
 //for loop that prints out pokemon and height to index.html
 
-for (let i= 0; i < pokemonList.length; i++) {
-  document.write (pokemonList[i].name + " - Height: " + pokemonList[i].height);
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " - Height: " + pokemonList[i].height);
 
-//Conditional if statement that differentiates pokemon by height
+  //Conditional if statement that differentiates pokemon by height
 
   if (pokemonList[i].height > 1) {
-      document.write (" - That is a big Pokemon!")
+    document.write(" - That is a big Pokemon!")
   } else if (pokemonList[i].height < 0.5) {
-    document.write (" - That is a small Pokemon!")
+    document.write(" - That is a small Pokemon!")
   }
 
-  document.write ("<br>");
+  document.write("<br>");
 }
