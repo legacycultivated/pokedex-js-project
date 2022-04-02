02427f79eece20942c42c31eebaec9af9640e48f
Lyman Chan <87409209+legacycultivated@users.noreply.github.com>
2022-04-01 20:20:21 -0700

    Update scripts.js
    updated indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 let pokemonRepository = (function() {
 
-  let pokemonList = [
-    {
+  let pokemonList = [{
       name: "Bulbasaur",
       height: 0.7,
       types: ['grass', 'poison']
@@ -31,14 +30,15 @@ let pokemonRepository = (function() {
   function getAll() {
     return pokemonList;
   }
-  function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
 
-    return {
-      getALl: getAll,
-      add: add
-    };
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getALl: getAll,
+    add: add
+  };
 
 
 
@@ -50,12 +50,12 @@ let pokemonRepository = (function() {
 
 //foreach loop Ex. 1.5
 
- pokemonRepository.getALl().forEach((pokemon) => {
-      document.write("<p>" + pokemon.name + " - Height: " + pokemon.height + "</p>");
- if(pokemon.height > 1) {
-   document.write( " - That is a big Pokemon!")
- } else if (pokemon.height < 0.5){
-   document.write(" - That is a small Pokemon!")
- }
+pokemonRepository.getALl().forEach((pokemon) => {
+  document.write("<p>" + pokemon.name + " - Height: " + pokemon.height + "</p>");
+  if (pokemon.height > 1) {
+    document.write(" - That is a big Pokemon!")
+  } else if (pokemon.height < 0.5) {
+    document.write(" - That is a small Pokemon!")
+  }
 
 });
